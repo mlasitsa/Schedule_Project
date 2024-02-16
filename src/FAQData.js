a24c10c19b2543a74531af1719380bc8cfd91589
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-02-15 21:36:52 -0600

    still working on faqPage

---
+++ src/FAQData.js
@@ -0,0 +1,28 @@
+export const FAQData = [
+    {
+        question: 'What is schedule.io?',
+        answer: 'An academic planning platform that simplifies course scheduling, enrollment, and progress tracking for educational institutions.',
+    },
+
+    {
+        question: 'How does the course scheduling feature work?',
+        answer: 'Utilizes a drag-and-drop interface for easy scheduling, considering room availability and academic needs.',
+    },
+    
+    {
+        question: 'Can students manage their enrollment through the platform?',
+        answer: 'Yes, students can register, view courses, and adjust schedules directly within the platform.',
+    },
+    
+    {
+        question: 'How does the platform track academic progress?',
+        answer: 'Monitors course completions and grades against degree requirements, offering real-time progress insights.',
+    },
+
+    {
+        question: 'How secure is student data on Schedule.io?',
+        answer: 'Security is our top priority. Schedule.io uses advanced encryption and security protocols to protect all student data and ensure privacy compliance.',
+    }
+    
+]
+
--- src/FAQPage/FAQPage.jsx
+++ src/FAQPage/FAQPage.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import '/src/FAQPage/FAQPage.css';
+import { FAQData } from '../FAQData';
+import { useState } from 'react';
 
 function FAQPage() {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const openAnswers = () => {
+        setIsClicked(!isClicked);
+      };
+
+      const definedTitle = (index) => {
+        if (isClicked == index) {
+            return setIsClicked(false);
+        }
+
+        setIsClicked(index);
+    }
+
     return (
         <div>
-            <h1>FAQ</h1>
+            <h1 className='faqpage-header'>FAQ</h1>
+
+            <div className='wrapper'>
+                <div className='accordion'>
+                    {FAQData.map((item, index) => (
+                        <div className='faqpage-item' onClick={() => definedTitle(index) }>
+                            <div className='faqpage-title'>
+                                <h2>{item.question}</h2>
+                                <span onClick={openAnswers} className='faqpage-dot'>+</span>
+                            </div>
+
+
+
+
+                            <div className={isClicked ? 'faqpage-content' : ''}>
+                                <p clas>{item.answer}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
 
 export default FAQPage;
+
+
+/* STILL WORKING ON IT */
