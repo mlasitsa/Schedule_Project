cbbcbeffdc810d3c1cdbcae824ef7c0f8668ac69
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-02-20 20:28:41 -0600

    updates

--- src/PrivacyPolicyPage/PrivacyPolicyPage.jsx
+++ src/PrivacyPolicyPage/PrivacyPolicyPage.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import '/src/PrivacyPolicyPage/PrivacyPolicyPage.css';
+import privacypolicypagelogo from '/privacypolicylogo.png';
+import privacypolicypagelogotwo from '/privacypolicypagelogotwo.png';
+import polictydata from './privacypolicy.data';
 
 function PrivacyPolicyPage() {
     return (
         <div>
             <h1 className='privacyPolicyPage-title'>PRIVACY POLICY</h1>
+            <p className='privacyPolicyPage-description'>We value your privacy and are committed to protecting your personal information. This policy outlines the types of information we collect, how it's used, and safeguarded</p>
+
+            <div className='privacyPolicyPage-answer'>
+                {polictydata.map((policy,index) => (
+                <div className='privacyPolicyPage-right'>
+                    <h4>{policy.policyName}</h4>
+                    <p>{policy.policyAnswer}</p>
+                </div>
+
+                
+                ))}
+            </div>
         </div>
     )
 }
---
+++ src/PrivacyPolicyPage/privacypolicy.data.js
@@ -0,0 +1,33 @@
+const policydata = [
+    {
+        policyName: 'Information Collection',
+        policyAnswer: 'Schedule.io collects information necessary for providing our academic planning services, including but not limited to names, email addresses, and academic interests',
+    },
+
+    {
+        policyName: 'Use of Information',
+        policyAnswer: 'The information collected is used to personalize your experience, improve our platform, and communicate important updates',
+    },
+
+    {
+        policyName: 'Information Sharing',
+        policyAnswer: 'We do not sell your information. Information may be shared with third parties only when necessary for providing our services or when legally required.',
+    },
+
+    {
+        policyName: 'User Rights',
+        policyAnswer: 'You have the right to access, correct, or delete your personal information at any time',
+    },
+
+    {
+        policyName: 'Data Security',
+        policyAnswer: 'We employ robust security measures to protect your information from unauthorized access or disclosure',
+    },
+
+    {
+        policyName: 'Policy Updates',
+        policyAnswer: 'We reserve the right to modify this privacy policy at any time, with changes effective upon posting to our platform',
+    },
+]
+
+export default policydata;
