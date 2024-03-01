7710e497da643d2ff79c20350764e3b1a8be41b0
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-02-29 20:35:54 -0600

    updates

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const cors = require("cors");
 const UsersModel = require('./models/Users')
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.json())
@@ -11,12 +12,6 @@ app.use(cors());
 
 mongoose.connect("mongodb+srv://mlasitsa:maxlas@schedulecluster.fdnhkev.mongodb.net/users")  
 
-
-app.post('/signup', (req, res) => {
-    UsersModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
-})
 
 app.post('/login', (req, res) => {
     const {email, password} = req.body;
@@ -33,6 +28,17 @@ app.post('/login', (req, res) => {
         }
     })
 })
+
+app.post('/signup', (req, res) => {
+    const {fullName, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        UsersModel.create({fullName, email, hash})
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+    }).catch(err => console.log(err.message))
+})
+
 
 app.listen(3001, () => {
     console.log("server is running")
